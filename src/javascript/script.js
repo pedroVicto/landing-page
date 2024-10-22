9df04b859f23b7911aede659521549ee79e7a870
pedroVicto <victorpedrosilva445@gmail.com>
2024-10-22 05:54:55 -0300

    create home, javascript,header

---
+++ src/javascript/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#mobile-btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile-btn').find('i').toggleClass('fa-x');
+    })
+}
+)
